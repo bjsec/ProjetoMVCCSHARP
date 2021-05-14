287b80ede32ab88839c0011d59337890310065ce
Bruno <ayres.sailing@gmail.com>
2021-05-14 17:50:01 -0300

    crud completo realizado

--- ProjetoMVCCSHARP/Startup.cs
+++ ProjetoMVCCSHARP/Startup.cs
@@ -37,7 +37,8 @@ namespace ProjetoMVCCSHARP
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ProjetoMVCCSHARPContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoMVCCSHARPContext")));
+                    options.UseMySql(Configuration.GetConnectionString("ProjetoMVCCSHARPContext"),builder =>
+                    builder.MigrationsAssembly("ProjetoMVCCSHARP")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
