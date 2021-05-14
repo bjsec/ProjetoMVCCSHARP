2070f6554261cc0861e99630edc2070f9013ca6f
Bruno <ayres.sailing@gmail.com>
2021-05-14 17:01:49 -0300

    crud scaffolding

---
+++ ProjetoMVCCSHARP/Data/ProjetoMVCCSHARPContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVCCSHARP.Models;
+
+namespace ProjetoMVCCSHARP.Data
+{
+    public class ProjetoMVCCSHARPContext : DbContext
+    {
+        public ProjetoMVCCSHARPContext (DbContextOptions<ProjetoMVCCSHARPContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoMVCCSHARP.Models.Department> Department { get; set; }
+    }
+}
