29846375429a3e1ee32dc04c918d8ba0e0d0f7d8
Bruno <ayres.sailing@gmail.com>
2021-05-16 16:02:09 -0300

    ancora para os próximos links

---
+++ ProjetoMVCCSHARP/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using ProjetoMVCCSHARP.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoMVCCSHARP.Models;
+
+namespace ProjetoMVCCSHARP.Services
+{
+    public class SellerService
+    {
+        private readonly ProjetoMVCCSHARPContext _context;
+
+        public SellerService(ProjetoMVCCSHARPContext context)
+        {
+            _context = context;
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
